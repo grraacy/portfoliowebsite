b2035a7aea5be82298e2a7ab5273ccd3875da8e9
zachary <zgadelson@comcast.net>
2026-04-05 18:16:48 -0700

    curved guh

---
+++ scripts/home.js
@@ -0,0 +1,15 @@
+document.getElementById("home").addEventListener("click", () => {
+    console.log("go HOME");
+})
+
+document.getElementById("work").addEventListener("click", () => {
+    console.log("back to work");
+})
+
+document.getElementById("aboutMe").addEventListener("click", () => {
+    console.log("all about me (guh)");
+})
+
+document.getElementById("contact").addEventListener("click", () => {
+    console.log("contact me wink wink");
+})
